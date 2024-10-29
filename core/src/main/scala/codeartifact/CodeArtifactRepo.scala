5a95fd0827a2d6fdf6e5ae6f452b117246653ca7
Hua Jiang <huajiang@tubi.tv>
2024-10-29 13:55:51 +0800

    build codeartifact client with the proper aws region

--- core/src/main/scala/codeartifact/CodeArtifactRepo.scala
+++ core/src/main/scala/codeartifact/CodeArtifactRepo.scala
@@ -7,6 +7,7 @@ case class CodeArtifactRepo(
   domain: String,
   host: String,
   owner: String,
+  region: String,
   url: String
 ) {
   def realm: String = s"$domain/$name"
@@ -14,34 +15,14 @@ case class CodeArtifactRepo(
 }
 
 object CodeArtifactRepo {
+  private val CodeArtifactUrl = "https://((.*)-(.*).d.codeartifact.(.*).amazonaws.com)/maven/(.*)".r
 
   def fromUrl(url: String): CodeArtifactRepo = {
-    if (url.isEmpty()) {
-      sys.error("""codeArtifactUrl not defined. assign this with codeArtifactUrl := "your-url"""")
+    url match {
+      case CodeArtifactUrl(host, domain, owner, region, repo) =>
+        CodeArtifactRepo(name = repo, domain = domain, host = host, owner = owner, region = region, url = url)
+      case _ =>
+        sys.error(s"Invalid codeArtifactUrl: $url")
     }
-
-    // Url looks like:
-    // https://<domain>-<owner>.d.codeartifact.us-west-2.amazonaws.com/maven/<repository>
-    val juri = new java.net.URI(url)
-
-    // Split on slashes, and get the last element: <repository>.
-    val name = juri.getPath().split('/').last
-
-    // Split on dots. Take the head, which is the <domain>-<owner> section.
-    // Split on dashes.
-    val host = juri.getHost()
-    val parts = host.split('.').head.split('-')
-    // Last element is <owner>.
-    val owner = parts.last
-    // The rest are the <domain>. Merge them together again.
-    val domain = parts.init.mkString("-")
-
-    CodeArtifactRepo(
-      name = name,
-      domain = domain,
-      host = host,
-      owner = owner,
-      url = url
-    )
   }
 }
